515c3ce65b21aeb8f3834f425fde3555a8081ac5
Peter De Permentier <peter.depermentier@smals.be>
2024-03-27 16:22:04 +0100

    Initial commit

--- duinroos/src/app/app-routing.module.ts
+++ duinroos/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false} // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
+
 export class AppRoutingModule { }
